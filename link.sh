8dfa1e4c7168317f3fa48c452c3c7070b4ec6fab
Tina Hinojosa <agustina.hinojosa@gmail.com>
2015-10-21 09:04:36 -0700

    adds script to set up links

---
+++ link.sh
@@ -0,0 +1,9 @@
+#RUN THIS SCRIPT INSIDE dotfiles/
+#Gonna start hard coding each file.
+#Then have it detect files.
+ln -s $HOME/.vimrc ./vimrc
+ln -s $HOME/.bashrc ./bashrc
+ln -s $HOME/.bash_profile ./bash_profile
+ln -s $HOME/.xinitrc ./xinitrc
+ln -s $HOME/.i3/config ./i3/config
+ln -s $HOME/.i3/i3status.conf ./i3/i3status.conf
