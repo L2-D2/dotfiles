ed6185e8bd6d3210b1441ce61bae622eb8499534
Tina Hinojosa <agustina.hinojosa@gmail.com>
2015-10-21 14:27:11 -0700

    puts links in correct syntax

--- link.sh
+++ link.sh
@@ -1,9 +1,10 @@
 #RUN THIS SCRIPT INSIDE dotfiles/
-#Gonna start hard coding each file.
-#Then have it detect files.
-ln -s $HOME/.vimrc ./vimrc
-ln -s $HOME/.bashrc ./bashrc
-ln -s $HOME/.bash_profile ./bash_profile
-ln -s $HOME/.xinitrc ./xinitrc
-ln -s $HOME/.i3/config ./i3/config
-ln -s $HOME/.i3/i3status.conf ./i3/i3status.conf
+
+#Gonna start with hard coding each file.
+#TODO: Have it detect files then link them.
+ln -s ./vimrc $HOME/.vimrc
+ln -s ./bashrc $HOME/.bashrc
+ln -s ./bash_profile $HOME/.bash_profile
+ln -s ./xinitrc $HOME/.xinitrc
+ln -s ./i3/config $HOME/.i3/config
+ln -s ./i3/i3status.conf $HOME/.i3/i3status.conf
